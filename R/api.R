f0624388d4e6e5a7aace92cd48c209999cb5a848
Jeremy Cusack <jeremy@okala.io>
2024-09-09 16:54:23 +0100

    Updated functions and get labels feature

--- R/api.R
+++ R/api.R
@@ -1,65 +1,63 @@
 library(tidyverse)
-# okala_url <- "https://dev.api.dashboard.okala.io/api/"
-
+#okala_URL <- "https://dev.api.dashboard.okala.io/api/"
 api_key <- "D2lfE2pxnrWI83daSqYqPcZDDSpwEIGT4lgNrOtv7ML5Qkk7qORwBmgvg7e46wd5MTuaVRwAMzaDuycrfH6Wuxy1Ti0PSFnHFeIF"
-#' Initiate root url and with api
+
+#' Initiate root URL with API key
 #'
-#' Creates a base URL object that can be used as base to call enpoints
-#' You need o get a project API key from our dashboard to use this function
+#' Creates a base URL object that can be used as a root to call endpoints.This requires a project API key, which can be obtained directly from the Okala dashboard.
 #'
-#' @param api_key a valid API key
-#' @return a list containing the root url and the api key
+#' @param api_key A valid API key
+#'
+#' @return This function returns a list containing the root URL and the API key
+#'
 #' @export
 auth_headers <- function(api_key){
-  okala_url <- "http://localhost:8000/api/"
+  #okala_url <- "http://localhost:8000/api/"
+  okala_url <- "https://dev.api.dashboard.okala.io/api/"
   root <- httr2::request(okala_url)
-  d = list(key = api_key,root = root)
+  d = list(key=api_key,
+           root=root)
   return(d)
 }
 
-# headers <- auth_headers(api_key)
+headers <- auth_headers(api_key)
 
-### Get summary station data frame
 #' Get project station metadata
 #'
-#' Get all of the station data assocated with your project
-#' For data types c("video","audio","image","eDNA")
+#' Retrieve all of the station data associated with your project, including video, audio, image, and eDNA data types.
 #'
-#' @param headers a valid API key
-#' @param datatype a character vector of data types c("video","audio","image")
-#' @return an sf object containing station metadata
+#' @param hdr A base URL provided and valid API key returned by the function \link{auth_headers}
+#' @param datatype A character vector of data types c("video","audio","image","eDNA")
+#'
+#' @return This function returns an sf object containing station metadata and geometry
 #' @export
-get_station_info <- function(headers,
-                             datatype=c("video","audio","image","eDNA"),
-                             showURL=F){
-
-  urlreq_ap <- httr2::req_url_path_append(headers$root,"getStations",datatype,headers$key)
+get_station_info <- function(hdr,
+                             datatype=c("video","audio","image","eDNA")){
+  urlreq_ap <- httr2::req_url_path_append(hdr$root,"getStations",datatype,hdr$key)
   preq <- httr2::req_perform(urlreq_ap)
   resp <- httr2::resp_body_string(preq)
-  # Geojson to sf
   geojson_response = geojsonsf::geojson_sf(resp)
   return(geojson_response)
-
 }
 
-# stations <- get_station_info(headers,datatype = "video")
+stations <- get_station_info(hdr=headers,datatype="video")
 
-### Get media assets for a given station
-#' Get media assets for a given project_system_record_id
+#' Retrieve media assets for a given project system record ID
 #'
-#' Get all of the station data assocated with your project
-#' For data types c("video","audio","image")
+#' Get all of the station data associated with your project. For data types c("video","audio","image")
 #'
-#' @param headers containing a valid API key
-#' @param datatype a character vector of data types c("video","audio","image")
-#' @return tibble of media assets for the given project_system_record
+#' @param hdr A base URL provided and valid API key returned by the function \link{auth_headers}
+#' @param datatype A character vector of data types c("video","audio","image","eDNA")
+#' @param psrID Unique project system ID for which the media assets will be retrieved
+#'
+#' @return This function returns a tibble of media assets for the specified project system record
+#'
 #' @export
-get_media_assets <- function(headers,
-                             datatype=c("video","audio","image"),
-                             project_system_record_id,
-                             showURL=F){
+get_media_assets <- function(hdr,
+                             datatype=c("video","audio","image","eDNA"),
+                             psrID){
 
-  urlreq_ap <- httr2::req_url_path_append(headers$root,"getMediaAssets",datatype,project_system_record_id,headers$key)
+  urlreq_ap <- httr2::req_url_path_append(hdr$root,"getMediaAssets",datatype,psrID,hdr$key)
   preq <- httr2::req_perform(urlreq_ap)
   resp <- httr2::resp_body_string(preq)
 
@@ -67,32 +65,31 @@ get_media_assets <- function(headers,
 
 }
 
-# media_labels <- get_media_assets(headers,datatype="video", project_system_record_id=stations$project_system_record_id[1])
+media_labels <- get_media_assets(hdr=headers,
+                                 datatype="video",
+                                 psrID=stations$project_system_record_id[1])
 
-### Get project labels for a given station
 #' Get project labels for either bioacoustics or camera
 #'
-#' Labels are derived by using either suggested labels on the platform or by manulaly
+#' Labels are derived by using either suggested labels on the platform or by manually
 #' adding labels from the wider database
 #'
-#' @param headers containing a valid API key
-#' @param label_type a character vector dictating the label type c('Bioacoustic','Camera')
-#' @return tibble of project labels
+#' @param hdr A base URL provided and valid API key returned by the function \link{auth_headers}
+#' @param labeltype A character vector specifying the label type ('Bioacoustic' or 'Camera')
+#'
+#' @return This function returns a tibble containing project labels
+#'
 #' @export
-
-get_project_labels <- function(headers,label_type = c('Bioacoustic','Camera'),
-
-                               showURL=F){
-
-  urlreq_ap <- httr2::req_url_path_append(headers$root,"getProjectLabels",label_type,headers$key)
+get_project_labels <- function(hdr,
+                               labeltype = c('Bioacoustic','Camera')){
+  urlreq_ap <- httr2::req_url_path_append(hdr$root,"getProjectLabels",labeltype,hdr$key)
   preq <- httr2::req_perform(urlreq_ap)
   resp <- httr2::resp_body_string(preq)
 
   return(jsonlite::fromJSON(resp) %>% tibble::as_tibble())
-
 }
 
-project_camera_labels <- get_project_labels(headers,label_type = 'Camera')
+project_camera_labels <- get_project_labels(hdr=headers,labeltype='Camera')
 
 test_labels <- media_labels %>% select(label_id, label_record_id, media_file_reference_location) %>% jsonlite::toJSON()
 
