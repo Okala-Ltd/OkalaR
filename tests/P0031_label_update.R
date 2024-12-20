82600b4f959e77801b7e45e8d0e0d225250a82e7
Cristobal <cristobal@okala.io>
2024-12-20 10:58:58 +0000

    data update

--- tests/P0031_label_update.R
+++ tests/P0031_label_update.R
@@ -1,11 +1,19 @@
 ## WCS label update ##
 ##
 
+## Libraries ####
+
 library(tidyverse)
 
+## Directory ####
 
+setwd("~/Documents/RStudio/OkalaR/")
+
+## API Functions ####
 
 source("~/Documents/RStudio/OkalaR/R/api.R")
+
+## API Key ####
 
 api_key <- "s92N22o5qqxGzoXW2qBgS8yTNMym6ktM3EUJtOtqh16hl6jy4vV0Jkw5JUdo3Be32STYFHLfuMFRXceF12vAwEPIkSgtMzg2vU3B"
 
@@ -34,7 +42,7 @@ project_camera_labels <- get_project_labels(hdr=headers,labeltype='Camera')
 wcs_media_labels <- media_labels %>%
   select(media_file_reference_location,segment_record_id,label_record_id, number_of_individuals)
 
-new_labels <- read_csv("~/Downloads/data-2024-12-19T13_55_40.946Z.CSV") %>%
+new_labels <- read_csv("/data/data-2024-12-19T13_55_40.946Z.CSV") %>%
   select(media_file_reference_location,label)
 
 labels <- read_csv("/Volumes/Temporary drop/WCS - Images/P0032_species_ID_list_FINAL.csv")
