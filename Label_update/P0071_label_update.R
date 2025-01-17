75adaf1467c2aa22f023de38fc7a0339e7b8a7f9
Cristobal <cristobal@okala.io>
2025-01-17 10:15:09 +0000

    update code

--- Label_update/P0071_label_update.R
+++ Label_update/P0071_label_update.R
@@ -41,9 +41,6 @@ media_labels <- get_media_assets(hdr=headers,
                                  datatype="audio",
                                  psrID=stations$project_system_record_id)
 
-media_labels %>% 
-  filter(label_id == "-1")
-
 # label_id: 106735 - label: Aves
 
 labelled_data <- tibble()
@@ -63,31 +60,27 @@ for (i in 1:18) {
 
 project_camera_labels <- get_project_labels(hdr=headers,labeltype='Bioacoustic')
 
-## French data set ####
-
 modified_media_labels <- media_labels %>%
   select(media_file_reference_location,segment_record_id,label_record_id,common_name)
 
-remove_data_2 <- remove_data %>% 
-  select(common_name) %>% 
-  left_join(modified_media_labels) %>% 
+remove_data_2 <- remove_data %>%
+  select(common_name) %>%
+  left_join(modified_media_labels) %>%
   mutate(label_id = "106735") %>%
   mutate(number_of_individuals = 1,
-         prediction_accuracy = 100) 
+         prediction_accuracy = 100)
 
 data_to_upload <- remove_data_2 %>%
   select(-c(common_name,media_file_reference_location)) %>%
   rename("segment_record_id_fk" = segment_record_id,
          "label_id_fk" = label_id)
 
-data_to_upload <- data_to_upload %>% 
+data_to_upload <- data_to_upload %>%
   slice(1:100)
 
 ## Push new labels ####
 
 push_new_labels(hdr=headers, submission_records = data_to_upload, chunksize=10)
-
-media_labels %>% filter(media_file_reference_location == "Cameras/PL9CT2/IMG_0608")
 
 ## segment_record_id is from the data set on the dashboard, I need to match the old file_path with the new one
 ## I need to find the proper label from project_camera_label (IUCN) to match it with the label_id
