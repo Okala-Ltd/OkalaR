b4c9c787b7e0467fcfac02d4bc47d9e1b0b1d773
AdamVarley30 <adamvarley1988@gmail.com>
2025-06-04 16:31:35 +0100

    cleaning

---
+++ R/okalaR-package.R
@@ -0,0 +1,7 @@
+#' okalaR: API wrapper for the Okala dashboard
+#'
+#' Provides functions to interact with the Okala dashboard API, including project, station, media, and label management.
+#'
+#' @docType package
+#' @name okalaR
+NULL
