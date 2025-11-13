677b1408f19e2c976797d60f2a48d7fd249d4b25
AdamVarley30 <adamvarley1988@gmail.com>
2025-11-13 16:56:20 +0000

    Add eDNA functions: check_edna_labels and upload_edna_records

--- R/okalaR-package.R
+++ R/okalaR-package.R
@@ -1,7 +1,10 @@
-#' okalaR: API wrapper for the Okala dashboard
-#'
-#' Provides functions to interact with the Okala dashboard API, including project, station, media, and label management.
-#'
-#' @docType package
-#' @name okalaR
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
